cd6c69c1e3aa808c37f7330d17b09a2c5184c12a
jess169 <jess.n169@hotmail.com>
2017-04-09 21:36:20 -0400

    mobile check and manual play

--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@ jQuery(function($) {
 	//Preloader
 	var preloader = $('.preloader');
 
+	var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+	var video = document.getElementById('myvideo');
+	video.play();
 if(played()==0 && !isMobile.any()){
 	document.getElementById('myvideo').addEventListener('ended',myHandler,false);
     function myHandler(e) {
